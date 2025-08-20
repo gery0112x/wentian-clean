41777f82383849f5b11393a97328c4c7bf03a416
gery0112x <gery0112@gmail.com>
2025-08-20 12:40:45 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true
+  // 不要設定 output:'export'，要保留 Serverless API。
+};
 export default nextConfig;
