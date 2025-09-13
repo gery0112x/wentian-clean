1e9d3a552fd81df729539ac3cbab5095afa728b7
gery0112x <gery0112@gmail.com>
2025-09-13 11:41:24 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,2 @@
-// wuji-sw v2 (debug)
-const SW_VER = 'wuji-sw-2';
-self.addEventListener('install', (evt) => {
-  console.log(`[SW ${SW_VER}] install`);
-  self.skipWaiting();
-});
-self.addEventListener('activate', (evt) => {
-  console.log(`[SW ${SW_VER}] activate`);
-  evt.waitUntil(self.clients.claim());
-});
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', () => self.clients.claim())
