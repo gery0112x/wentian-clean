168f89aed73f590a0a43c15b9b35c7cc04e41bee
gery0112x <gery0112@gmail.com>
2025-09-13 18:56:13 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,13 @@
-self.addEventListener('install', () => self.skipWaiting())
-self.addEventListener('activate', () => self.clients.claim())
+// public/sw.js
+self.addEventListener('install', (event) => {
+  console.log('[sw] install');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('[sw] activate');
+  event.waitUntil(self.clients.claim());
+});
+
+// 空的 fetch handler 也可以，重點是存在，滿足安裝條件
+self.addEventListener('fetch', (event) => {});
