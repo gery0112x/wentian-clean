f4cf6667ce18bed76bd65e88d92de323b3216b60
gery0112x <gery0112@gmail.com>
2025-09-15 19:54:26 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
-  await supabase.auth.getSession(); // 確保 cookie 正常維護
+  await supabase.auth.getSession(); // 維持 sb-* cookie 生命週期
   return res;
 }
 export const config = { matcher: ['/((?!_next|.*\\..*).*)'] };
